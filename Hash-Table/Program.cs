1f3eb44c9109cf9f1794fe450c90596fa22e9d8d
pampapathi73 <pampapathik32@gmail.com>
2021-06-14 19:48:46 +0530

    [pampapathi] Added: Find Frequency Of Words In sentence

--- Hash-Table/Program.cs
+++ Hash-Table/Program.cs
@@ -7,6 +7,18 @@ namespace Hash_Table
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome to hash table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash2 = hash.Get("2");
+            Console.WriteLine("The value for second index is: {0} ", hash2);
+            string hash1 = hash.Get("1");
+            Console.WriteLine("The value for First index is: {0} ", hash1);
         }
     }
-}
+    }
+
